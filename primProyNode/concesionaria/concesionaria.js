652b7bf02acd96c43b3b55bf9d5b2b91b423a794
franciscocesare <pako_cesare@hotmail.com>
2022-05-27 16:10:32 -0300

    work in class, and testing express generator

--- primProyNode/concesionaria/concesionaria.js
+++ primProyNode/concesionaria/concesionaria.js
@@ -48,19 +48,19 @@ let concesionaria = {
   },
 };
 
-//console.log(concesionaria.buscarAuto("JJK116")); //no encuentra
-// console.log(concesionaria.buscarAuto("APL123")); //si encuentra
-//console.log(concesionaria.venderAuto("APL123"));
+console.log(concesionaria.buscarAuto("JJK116")); //no encuentra
+ console.log(concesionaria.buscarAuto("APL123")); //si encuentra
+console.log(concesionaria.venderAuto("APL123"));
 //console.log(concesionaria.venderAuto("GRA888"));
 // console.log(concesionaria.buscarAuto("APL123")); //si encuentra
 // console.log(concesionaria.autosParaLaVenta());
-console.log(concesionaria.autosNuevos());
-console.log(concesionaria.listaDeVentas());
-console.log(
-  concesionaria.autosQuePuedeComprar({
-    nombre: "Juan",
-    capacidadDePagoEnCuotas: 20000,
-    capacidadDePagoTotal: 100000,
-  })
-);
+// console.log(concesionaria.autosNuevos());
+// console.log(concesionaria.listaDeVentas());
+// console.log(
+//   concesionaria.autosQuePuedeComprar({
+//     nombre: "Juan",
+//     capacidadDePagoEnCuotas: 20000,
+//     capacidadDePagoTotal: 100000,
+//   })
+// );
 //console.log(concesionaria.totalDeVentas());

56d70f17e12630ec4de5ffc878e80963350dccdb
franciscocesare <pako_cesare@hotmail.com>
2022-06-01 00:56:55 -0300

    work in MercadoLiebre, adding some styles yet

--- intro-html/app.js
+++ intro-html/app.js
@@ -2,13 +2,17 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+const PORT = process.env.PORT || 3030;
+
 const publicPath = path.resolve(__dirname, './public')
 app.use(express.static(publicPath))
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000');
-})
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
 })
